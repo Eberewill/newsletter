30924c88799dc7e5a56702add2f683d1a04fc607
Eberechi <eberewill@gmail.com>
2021-06-25 09:32:32 +0100

    deployment fix

--- frontend/src/screen/VerifyEmail.js
+++ frontend/src/screen/VerifyEmail.js
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const VerifyEmail = ({ history }) => {
-  const verifyUserEmail = (ref) => {
+  const [loading, setLoading] = useState(false);
+  const verifyUserEmail = async (ref) => {
     return new Promise((resolve, reject) => {
+      setLoading(true);
       const body = { ref: ref };
       const { data } = axios.post(`/api/v1/newslatter/verify/`, body);
+      setLoading(false);
       if (data) {
         resolve(data);
       } else {
@@ -25,7 +28,7 @@ const VerifyEmail = ({ history }) => {
       .catch((err) => alert(err));
   }, []);
 
-  return <div></div>;
+  return <div>{loading && <h3> Loading...</h3>}</div>;
 };
 
 export default VerifyEmail;
