38273c314bc4888083fb11de857807ef648054ff
Eberechi <eberewill@gmail.com>
2021-06-25 09:14:42 +0100

    deployment fix

--- frontend/src/screen/VerifyEmail.js
+++ frontend/src/screen/VerifyEmail.js
@@ -2,9 +2,16 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const VerifyEmail = ({ history }) => {
-  const verifyUserEmail = (ref, cb) => {
-    axios.post(`/api/v1/newslatter/verify/`, ref);
-    cb();
+  const verifyUserEmail = (ref) => {
+    return new Promise((resolve, reject) => {
+      const body = { ref: ref };
+      const { data } = axios.post(`/api/v1/newslatter/verify/`, body);
+      if (data) {
+        resolve(data);
+      } else {
+        reject("something went wrong while sending request");
+      }
+    });
   };
 
   const ref = new URL(window.location.href).searchParams.get("ref");
@@ -13,9 +20,9 @@ const VerifyEmail = ({ history }) => {
     if (!ref) {
       history.push("/");
     }
-    verifyUserEmail(ref, () => {
-      window.location.replace("https://www.google.com");
-    });
+    verifyUserEmail(ref)
+      .then(window.location.replace("https://www.google.com"))
+      .catch((err) => alert(err));
   }, []);
 
   return <div></div>;
--- test.js
+++ test.js
@@ -3,26 +3,40 @@ const get_top_features = (
   possibleFeatures,
   featureRequests
 ) => {
-  const totalObj = [];
-  const counter = [];
+  //map the possible features into a list of [feature, featureCount]. all count initially starting at 0
+  let possibleFeaturesWithCount = possibleFeatures.map((element) => [
+    element,
+    0,
+  ]);
 
-  //split the featurerequets and convert the featureRequests to individual Array
-  featureRequests.forEach((element) => {
-    let araayItems = element.split(" ");
-    araayItems.forEach((element) => {
-      counter.push(element);
-    });
-  });
+  featureRequests.forEach((featureRequestItem) => {
+    //which possible features does this feature request fall into? Find it and increment the feature count.
 
-  counter.forEach((lst) => {
-    possibleFeatures.forEach((pf) => {
-      if (lst == pf) {
-        totalObj.push({ lst });
+    for (let index = 0; index < possibleFeaturesWithCount.length; index += 1) {
+      let possibleFeatureWithCount = possibleFeaturesWithCount[index];
+
+      //use basic string search (case insensitive) to check if feature request contains a possible featue keyword.
+      let possibleFeatureKeyword = possibleFeatureWithCount[0];
+
+      if (
+        featureRequestItem
+          .toLowerCase()
+          .includes(possibleFeatureKeyword.toLowerCase())
+      ) {
+        possibleFeaturesWithCount[index][1]++;
       }
-    });
+    }
   });
 
-  return totalObj;
+  possibleFeaturesWithCount.sort((first, second) => {
+    return second[1] - first[1];
+  });
+
+  let sortedPossibleFeatures = possibleFeaturesWithCount.map(
+    (element) => element[0]
+  );
+
+  return sortedPossibleFeatures.slice(0, topFeaturesNumber);
 };
 
 const possibleFeatures = [
@@ -40,7 +54,6 @@ const featureRequests = [
   "Waterproof and increased battery are my top two",
   "I want to take my Kindle into the shower. Waterproof please waterproof!",
   "I wanna make my Kindle hover on my desk",
-  "How about a solar Kindle!",
 ];
 
 console.log(get_top_features(3, possibleFeatures, featureRequests));
